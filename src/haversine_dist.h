c6fe907c3eebf92d024b84adf69788615278b130
Your Name <smarinardila.stats@gmail.com>
2026-07-18 10:21:47 +1000

    clean up

--- src/haversine_dist.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAVERSINE_DIST_H
-#define HAVERSINE_DIST_H
-
-#include <RcppEigen.h>
-
-Rcpp::NumericMatrix haver_dist(Rcpp::NumericMatrix & coords);
-
-#endif  // HAVERSINE_DIST_H
--- src/lambda_ratio.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAMBDA_RATIO_H
-#define LAMBDA_RATIO_H
-
-#include <RcppEigen.h>
-
-double lambda_ratio_log_l(const Eigen::VectorXd & center_lambda,
-                          const Eigen::MatrixXd & cov_lambda);
-
-#endif  // LAMBDA_RATIO_H
