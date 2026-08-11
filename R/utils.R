d3314505ad50df2979e51d2dc8e63cc1242f5daf
Santiago Marin <55192971+marinsantiago@users.noreply.github.com>
2026-08-11 15:19:42 +1000

    dynclusts 0.1.0

--- R/utils.R
+++ R/utils.R
@@ -113,17 +113,15 @@ input.validation.dynclust <- \(y, coords, X, theta.0, sigma2.0, a.0, b.0,
 }
 
 input.validation.dynclust.sim <- \(n.obs, time.points, n.clusts, n.covariates,
-                                   tau2, rho2, varphi, sigma2, prob.jump, 
-                                   prob.pow, balanced, spatial.effect,
-                                   covariates.effect) {
+                                   country, tau2, rho2, varphi, sigma2, pi.stay, 
+                                   balanced, spatial.effect, covariates.effect){
   
-  p.scalars <- c("tau2", "rho2", "varphi", "sigma2", "prob.pow")
-  for (s in p.scalars) {
+  for (s in c("tau2", "rho2", "varphi", "sigma2")) {
     if (not.psc(get(s))) stop(paste(s, " must be a positive scalar"))
   }
   integers <- c("n.obs", "time.points", "n.covariates", "n.clusts")
   for (i in integers) if (not.int(get(i))) stop(paste(i, " must be an integer"))
   logicals <- c("balanced", "spatial.effect", "covariates.effect")
   for (l in logicals) if (not.logic(get(l))) stop(paste(l, " must be logical"))
-  if (not.unit(prob.jump)) stop("prob.jump must be in (0, 1)")
+  if (not.unit(pi.stay)) stop("pi.stay must be in (0, 1)")
 }
